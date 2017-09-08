8b81800f54afc3dcd058583b9a973aae682cbdd1
Victor Dozal <v.dozal@live.com>
2017-09-08 08:50:26 -0500

    Initial commit. Started with react-slingshot boilerplate

---
+++ src/components/App.js
@@ -0,0 +1,36 @@
+/* eslint-disable import/no-named-as-default */
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Route } from 'react-router';
+import { Switch, NavLink } from 'react-router-dom';
+import HomePage from './HomePage';
+import NotFoundPage from './NotFoundPage';
+
+// This is a class-based component because the current
+// version of hot reloading won't hot reload a stateless
+// component at the top-level.
+
+class App extends React.Component {
+  render() {
+    const activeStyle = { color: 'blue' };
+    return (
+      <div>
+        <div>
+          <NavLink exact to="/" activeStyle={activeStyle}>Home</NavLink>
+        </div>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          {/* <Route path="/fuel-savings" component={FuelSavingsPage} />
+          <Route path="/about" component={AboutPage} /> */}
+          <Route component={NotFoundPage} />
+        </Switch>
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  children: PropTypes.element
+};
+
+export default App;
