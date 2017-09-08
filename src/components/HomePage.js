eb1d74a1069066d138b59d102f18c6351af25e92
Victor Dozal <v.dozal@live.com>
2017-09-08 11:58:19 -0500

    Added basic search form in home page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import {
+  FormGroup, FormControl, InputGroup, Button
+} from 'react-bootstrap';
 // import { Link } from 'react-router-dom';
 
 const HomePage = () => {
   return (
     <div>
       <h1>Autolist Search</h1>
+      <FormGroup>
+        <InputGroup>
+          <FormControl type="text" />
+          <InputGroup.Button>
+            <Button>Search</Button>
+          </InputGroup.Button>
+        </InputGroup>
+      </FormGroup>
     </div>
   );
 };
