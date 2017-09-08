1f11035944c983965e92b3c66e2f9a450bc6c2da
Victor Dozal <v.dozal@live.com>
2017-09-08 12:39:47 -0500

    Updated routes with new folder structure

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 
 import App from './components/App';
-import HomePage from './components/HomePage';
-import SearchPage from './components/SearchPage';
-import VehiclePage from './components/VehiclePage';
+import HomePage from './components/home/HomePage';
+import SearchPage from './components/search/SearchPage';
+import VehiclePage from './components/vehicle/VehiclePage';
 import NotFoundPage from './components/NotFoundPage';
 
 export default (
